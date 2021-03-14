f9a0fc56777f74bac2ef76447d39bf552f45d6c1
Miqueias Lima <miqueiaslima@outlook.com>
2021-03-14 12:10:35 -0300

    Criação das Rotas do Animal

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const routes = express.Router();
 const Usuarios = require('./controllers/usuarioController');
 
-
+//Rotas de usuários
 routes.get('/',Usuarios.index);
 routes.get('/api/usuarios',Usuarios.index);
 routes.get('/api/usuarios.details/:_id',Usuarios.details);
 routes.post('/api/usuarios',Usuarios.create);
 routes.delete('/api/usuarios/:_id',Usuarios.delete);
 routes.put('/api/usuarios',Usuarios.update);
+
+
+//Rotas de Animal
+
 
 
 module.exports = routes;
