cf575dd7d08ee0bde1bade70e47c21884eb55e6d
Miqueias Lima <miqueiaslima@outlook.com>
2021-03-15 22:00:42 -0300

    Adicionando Template React do Projeto

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import Inicio from './pages/client/painel';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <header className="App-header">
+        <h1>Adoção de PETS</h1>
+  </header>*/}
+
+    <Inicio/>
+
+    </div>
+  );
+}
+
+export default App;
