5ddc2deb8f69605b39615beb3b3ca1826f7b06e3
Miqueias Lima <miqueiaslima@outlook.com>
2021-03-23 17:21:14 -0300

    Criando Template p/ Listagem de Animais

--- client/src/pages/admin/animais/index.js
+++ client/src/pages/admin/animais/index.js
@@ -1,11 +1,74 @@
 import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function AnimaisListagem() {
+import Box from '@material-ui/core/Box';
+
+
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+
+import Copyright from '../../../componnents/footer-admin';
+
+import MenuAdmin from '../../../componnents/menu-admin';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+ 
+  title: {
+    flexGrow: 1,
+  },
+
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+ 
+}));
+
+export default function AnimaisList() {
+  const classes = useStyles();
+  
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   return (
-    <div>
-        <h1>Listagem de Animais</h1>
-
+    <div className={classes.root}>
+      
+      <MenuAdmin title={'Lista de Animais'}/>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item sm={12}>
+              <h2>Listagem de Animais</h2>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={12}>
+                    
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      </main>
     </div>
   );
 }
-
