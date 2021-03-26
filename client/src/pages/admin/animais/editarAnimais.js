fbc5c41927be94bd6d046ce3e705c375595e32dc
Miqueias Lima <miqueiaslima@outlook.com>
2021-03-25 23:42:10 -0300

    Criando Template p/ Atualizar Cadastro de Animais

--- client/src/pages/admin/animais/editarAnimais.js
+++ client/src/pages/admin/animais/editarAnimais.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function EditarAnimal() {
-  return (
-    <div>
-        <h1>Atualização de Animais</h1>
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
 
-    </div>
-  );
-}
+import api from '../../../services/api';
+import Copyright from '../../../componnents/footer-admin';
+import MenuAdmin from '../../../componnents/menu-admin';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+ 
+  title: {
+    flexGrow: 1,
+  },
+
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+ 
+}));
