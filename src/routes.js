44687069689ab0e49fbd4a6c2abd6ed51eaad03d
Miqueias Lima <miqueiaslima@outlook.com>
2021-03-29 22:34:54 -0300

    Atualizando rotas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-const Usuarios = require('./controllers/usuarioController');
 const Animal = require('./controllers/animalController');
-
-//Rotas de usuários
-routes.get('/',Usuarios.index);
-routes.get('/api/usuarios',Usuarios.index);
-routes.get('/api/usuarios.details/:_id',Usuarios.details);
-routes.post('/api/usuarios',Usuarios.create);
-routes.delete('/api/usuarios/:_id',Usuarios.delete);
-routes.put('/api/usuarios',Usuarios.update);
 
 
 //Rotas de Animal
