dee4bf7dd579a32f49b6ea523d2f5456c1f3d052
Miqueias Lima <miqueiaslima@outlook.com>
2021-04-03 19:23:51 -0300

    Adicionando Arquivo de Estilização para o Menu do Admin

--- client/src/componnents/menu-admin.js
+++ client/src/componnents/menu-admin.js
@@ -8,12 +8,12 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import { makeStyles } from '@material-ui/core/styles';
+
 import clsx from 'clsx';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const drawerWidth = 240;
+import useStyles from '../estilos/menuAdmin';
 
 
 
